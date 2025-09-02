7c777869cb5c5730763a1ae722c2f1f96fa71377
Светлана <autizm6kola@yandex.com>
2025-09-02 18:24:14 +0300

    Ошибки

--- src/App.js
+++ src/App.js
@@ -457,7 +457,7 @@ function App() {
 
   useEffect(() => {
     setLoading(true);
-    fetch(process.env.PUBLIC_URL + '/tasks_two.json')
+    fetch(process.env.PUBLIC_URL + '/tasks_three.json')
       .then((res) => res.json())
       .then((data) => {
         setAllTasks(data);
