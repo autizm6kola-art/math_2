26b2a696a3bb2fef2cb8c3ed07578ca3daf0ed38
Светлана <autizm6kola@yandex.com>
2025-09-13 11:09:56 +0300

    Название json

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
 
   useEffect(() => {
     setLoading(true);
-    fetch(process.env.PUBLIC_URL + '/tasks_for.json')
+    fetch(process.env.PUBLIC_URL + '/tasks_five.json')
       .then((res) => res.json())
       .then((data) => {
         setAllTasks(data);
